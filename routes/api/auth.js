dee0f8ebf4acd8c6ab9ba10b6965dda442521f15
Trey <tajohns22@gmail.com>
2020-02-02 18:43:41 -0600

    added proile route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const bcrypt = require("bcryptjs");
-const {check, validationResult} = require("express-validator/check");
+const {check, validationResult} = require("express-validator");
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
 //@route    GET api/auth
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
-const {check, validationResult} = require("express-validator/check");
+const {check, validationResult} = require("express-validator");
 //get the user model
 const User = require("../../models/User");
 //@route    POST api/users
